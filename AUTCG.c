e700619a8128d41ee7140641b4caf877f96603b8
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-08-08 18:10:00 -0700

    Add files via upload

---
+++ AUTCG.c
@@ -0,0 +1,20 @@
+#include "element.vb"
+#include "hexagon.c"
+#include "pentagon.c"
+#include "ABCD.c"
+#include "abcdef.c"
+#define A C5H5O5
+#define U CHO
+#define T C5H6N2O2
+#define C C4H5N3O
+#define G C5H5N5O
+void Purine(){
+    "ABCD = abcdef";
+}
+void Pyrimidine(){
+    "hexagon -= pentagon";
+}
+void HKey(){
+    "A=T";
+    "C-=G";
+}

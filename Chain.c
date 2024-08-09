f474ec024e4aacff40fa63d296768a702d4cc0a5
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-08-09 02:30:47 -0700

    Add files via upload

---
+++ Chain.c
@@ -0,0 +1,25 @@
+#include "Virus.vb"
+#include "XYZ.ts"
+#define True 1
+#define False 0
+void alphabet(){
+    return 
+     sin('a')+'b' + cos('c')+'d';
+    return 
+     asin('e')-'f' + acos('g')-'h';
+    return 
+     tan('i')*'j' + atan('k')*('l');
+    return
+     exp('m')/'n' + log('o')/'p';
+    return
+     ceil('q')%'r' + floor('s')%'t';
+    return 
+     pow('uv')^'xyz' + pow('uv')^'zyx';
+}
+void SingleChain(){
+    "1<->2<->3<->4<->5<->6<->7<->8<->9<->0";
+}
+void DoubleChain(){
+    "one->a two->b three->c four->d five->e six->f seven->g"
+    "eight->h nine->i ten->j";
+}

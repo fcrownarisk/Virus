46c2463b1de4988b2cb4c6eef0261e735cca78fc
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-08-09 22:12:48 -0700

    Update bit.c

--- bit.c
+++ bit.c
@@ -19,10 +19,35 @@ const seven = "00000111";
 const eight = "00001000";
 const nine = "00001001";
 const ten = "00001010";
+    
 const a = "00001011";
 const b = "00001100";
 const c = "00001101";
 const d = "00001110";
 const e = "00001111";
 const f = "00010000";
+    
+const g = "00010001";
+const h = "00010010";
+const i = "00010011";
+const j = "00010100";
+const k = "00010101";
+const l = "00010111";
+    
+const m = "00011000";
+const n = "00011001";
+const o = "00011010";
+const p = "00011011";
+const q = "00011111";
+const r = "00100000";
+    
+const s = "00100001";
+const t = "00100010";
+const u = "00100011";
+const v = "00100100";
+const w = "00100101";
+    
+const x = "00100111";
+const y = "00101000";
+const z = "00101001";
 }

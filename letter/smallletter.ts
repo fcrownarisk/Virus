5729f986ba8a6cb8f2a88db01c9ad07442e95707
W <155938693+fcrownarisk@users.noreply.github.com>
2025-08-17 10:32:01 +0700

    Update smallletter.ts

--- letter/smallletter.ts
+++ letter/smallletter.ts
@@ -19,7 +19,7 @@ const o = '|=|'
 const p = '||\/'
 
 const q = '/\||'
-const r = '|//'
+const r = '|/\'
 const s = '-/\_'
 const t = '-|/'
 
@@ -31,3 +31,4 @@ const x = '\\//'
 const y = '\//'
 const z = '-/_'
 console.log(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+

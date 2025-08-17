b07aa5ecb16715dfd875dbc9cefaf11e2628270d
W <155938693+fcrownarisk@users.noreply.github.com>
2025-08-17 10:28:35 +0700

    Update abcdefgh.c

--- abcdefgh.c
+++ abcdefgh.c
@@ -1,11 +1,12 @@
-#define a |=\
-#define b |=/
-#define c |=
-#define d =|
-#define e |\/|
-#define f |/\|
-#define g |\|
-#define h |/|
 void abcdefgh(){
+  a = '|='
+  b = '=|'
+  c = '|=/'
+  d = '|=\'
+  e = '|\/|'
+  f = '|/\|'
+  g = '|\|'
+  h = '|/|'
   "a + b + c + d -> e + f + g + h";.
 }
+
